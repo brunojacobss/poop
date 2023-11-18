68b2904205698272bca161f243e83a04126eb4a4
Bruno Jacobs <brunojacobsschulz@gmail.com>
2023-11-18 15:09:50 -0500

    Add Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import {sanityIntegration} from "@sanity/astro";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanityIntegration({
+    projectId: "25mghe5s",
+    dataset: "production",
+    useCdn: false,
+  })]
+});
