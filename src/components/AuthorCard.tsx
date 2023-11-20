8910b4733405fc61e98f84bf33f3b2d387ad705e
Bruno Jacobs <brunojacobsschulz@gmail.com>
2023-11-19 20:01:45 -0500

    Fix mobile view

--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -11,39 +11,41 @@ export function AuthorCard(props: AuthorCardProps): JSX.Element {
   const medal = props.position === 1 || props.position === 2;
   return (
     <div className="w-full bg-gradient-to-r dark:from-[#1e293b] dark:to-[#020817] from-gray-300 to-white rounded-lg">
-      <div className="flex flex-row items-center justify-between p-8 w-full h-full">
+      <div className="flex flex-row items-center justify-between sm:p-8 p-4 w-full h-full">
         <div
           className={cn('', {
             'bg-yellow-300': props.position === 1,
             'bg-gray-500': props.position === 2,
-            'w-24 h-24': props.position === 1,
-            'w-16 h-16': props.position === 2,
+            'sm:w-24 sm:h-24': props.position === 1,
+            'sm:w-16 sm:h-16': props.position === 2,
+            'w-12 h-12': props.position === 1,
+            'w-8 h-8': props.position === 2,
             'rounded-full': medal,
             'flex items-center justify-center': medal,
-            'pl-6': !medal,
+            'sm:pl-6': !medal,
           })}
         >
           <p
-            className={cn('font-bold text-foreground text-2xl', {
+            className={cn('font-bold text-foreground sm:text-2xl text-lg', {
               'text-black': medal,
-              'text-6xl': props.position === 1,
-              'text-3xl': props.position === 2,
+              'sm:text-6xl': props.position === 1,
+              'sm:text-3xl': props.position === 2,
             })}
           >
             {props.position}
           </p>
         </div>
-        <h1 className="text-2xl font-bold">{props.name}</h1>
+        <h1 className="sm:text-2xl text-sm font-bold">{props.name}</h1>
         <div className="border rounded-full border-foreground flex items-center pl-4 pr-2 pt-2 pb-3">
-          <span className="text-foreground font-bold text-xl ">
+          <span className="text-foreground font-bold sm:text-xl text-sm">
             {props.poopCount}{' '}
           </span>
-          <span className="text-foreground text-xl">💩</span>
+          <span className="text-foreground sm:text-xl text-sm">💩</span>
         </div>
         <img
           className={cn('w-16 h-16 rounded-full', {
-            'w-32 h-32': props.position === 1,
-            'w-24 h-24': props.position === 2,
+            'sm:w-32 sm:h-32': props.position === 1,
+            'sm:w-24 sm:h-24': props.position === 2,
           })}
           src={
             props.imageUrl ||
