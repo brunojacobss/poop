83cf52a15669ec5349310d44381d1c8014831175
Bruno Jacobs <brunojacobsschulz@gmail.com>
2023-11-23 19:43:30 -0500

    Fix styles

--- src/components/ProgressionChart.tsx
+++ src/components/ProgressionChart.tsx
@@ -16,7 +16,8 @@ type Props = {
 
 export default function ProgressionChart({ chartData }: Props) {
   return (
-    <div className="mt-16 sm:h-96 h-48">
+    <div className="sm:mt-24 mt-16 sm:h-96 h-48">
+      <h1 className=" sm:text-2xl text-center text-lg mb-8">Progresión</h1>
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart data={chartData} width={100}>
           <defs>
